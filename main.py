b24838763b3d470cf1506680816ec1a47f376d3e
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2023-09-17 17:08:48 +0300

    1.5.1 update

--- main.py
+++ main.py
@@ -27,17 +27,16 @@ def funcs_print():
         |   ,/    |  | '.'|'   | '.'||  :  :_:,'|   | '  ,/ /  /  ,.  |'   | '.'|'  : |--'  
         '---'     ;  :    ;|   :    :|  | ,'    ;   : ;--' ;  :   .'   \   :    :;  |,'     
                   |  ,   /  \   \  / `--''      |   ,/     |  ,     .-./\   \  / '--'       
-                   ---`-'    `----'             '---'       `--`---'     `----'                            1.5
+                   ---`-'    `----'             '---'       `--`---'     `----'                            1.5.1
                                                                                     
          {blue}                                                   Functions by Ma3rX, Python_UT, Tevajs.
                                                                 With you after first SVC vs MARCO war!{yellow}
         [1] Хак аккаунтов(мултипарт)
         [2] Кик участников из команды
-        [3] Захватывающий портал(cookies)
-        [4] Генератор паролей
-        [5] Проверка паролем
-        [6] Деанон клуба
-        [7] Выход
+        [3] Генератор паролей
+        [4] Проверка паролем
+        [5] Деанон клуба
+        [6] Выход
         
     """
 
@@ -70,32 +69,29 @@ def start():
 
         elif func == '3':
             os.system('cls')
-            print(cyanlight + "Захватывающий портал. Только для fish224.")
-            check.trojan()
-            check.link_spoof()
-            print(green + "[+] Готово.")
-            input(magenta + "[?] Продолжить? ")
-            os.system('cls')
-            funcs_print()
-            start()
+            print(cyanlight + "Генерация паролей.")
+            choose_4 = input(magenta + "Генерация паролей может вызвать теоретическое снижение производительности вашего компьютера. Хотите продолжить? [y/N]")
+            if choose_4 == "y" or choose_4 == "Y":
+                with open("passwords.txt", "w") as passwords_:
+                    my_list = wordlist.start()
+                    for password in my_list:
+                        passwords_.write(password.replace("'", "") + '\n')
+                with open("passwords.txt", "r") as passwords_:
+                    for password in passwords_.read():
+                        print(password)
+                print(green + "[+] Готово.")
+                input(magenta + "[?] Продолжить? ")
+                os.system('cls')
+                funcs_print()
+                start()
+
+            else:
+                os.system('cls')
+                funcs_print()
+                start()
+
 
         elif func == '4':
-            os.system('cls')
-            print(cyanlight + "Генерация паролей.")
-            with open("passwords.txt", "w") as passwords_:
-                my_list = wordlist.start()
-                for password in my_list:
-                    passwords_.write(password.replace("'", "") + '\n')
-            with open("passwords.txt", "r") as passwords_:
-                for password in passwords_.read():
-                    print(password)
-            print(green + "[+] Готово.")
-            input(magenta + "[?] Продолжить? ")
-            os.system('cls')
-            funcs_print()
-            start()
-
-        elif func == '5':
             os.system('cls')
             print(cyanlight + "Проверка участников по паролю")
             password_check.start(password=input(green + "Введите пароль, которым вы хотите проверить клуб: " + magenta),
@@ -107,7 +103,7 @@ def start():
             funcs_print()
             start()
 
-        elif func == '6':
+        elif func == '5':
             os.system('cls')
             print(cyanlight + "Получение информации о всех участниках клуба")
             team_deanon.deanon(team=input(yellow + "Введите ID клуба: " + magenta))
@@ -119,7 +115,7 @@ def start():
             start()
 
 
-        elif func == '7':
+        elif func == '6':
             os.system('cls')
             print("--------------Выход--------------")
             exit()

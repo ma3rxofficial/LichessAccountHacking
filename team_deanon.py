e2091291d80beeb357dfc92793ca8b70cd00876a
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2023-11-08 21:50:53 +0300

    1.8.0 patch (team_deanon.py)

--- team_deanon.py
+++ team_deanon.py
@@ -5,7 +5,7 @@ from parser import *
 
 
 def deanon_default(team):
-    r = requests.get(f'https://{SERVER}/api/{TEAM}/' + team + '/users')
+    r = requests.get(f'https://{SERVER}/api/{TEAM}/' + team + f'/{USERS}')
     data = r.json(cls=ndjson.Decoder)
 
     print(green + "[+] Список пользователей получен!")
@@ -23,7 +23,7 @@ def deanon_multipart(team_list):
         print(team_list.split())
         for team in team_list.split():
             print(team)
-            r = requests.get(f'https://{SERVER}/api/{TEAM}/' + team + '/users')
+            r = requests.get(f'https://{SERVER}/api/{TEAM}/' + team + f'/{USERS}')
             data = r.json(cls=ndjson.Decoder)
 
             print(green + f"[+] Список пользователей получен!")
@@ -50,10 +50,10 @@ def start():
         deanon_default(team=input(magenta + "[?] ID команды: "))
 
     elif answer == "multipart":
-        deanon_multipart(team_list=input(magenta + "Введите файл с список клубов(через пробел): "))
+        deanon_multipart(team_list=input(magenta + "Введите файл со список клубов(через пробел): "))
 
     elif answer == "MULTIPART":
-        deanon_multipart(team_list=input(magenta + "Введите файл с список клубов(через пробел): "))
+        deanon_multipart(team_list=input(magenta + "Введите файл со список клубов(через пробел): "))
 
 if __name__ == "__main__":
     start()
